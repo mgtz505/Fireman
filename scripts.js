2b41b01a49c61577316a734953ecab26331d2964
mgtz505 <gutierrez.marcusc@gmail.com>
2021-02-23 15:37:41 -0500

    Match is almost successful, still working on addressing incorrect guesses

--- scripts.js
+++ scripts.js
@@ -13,7 +13,6 @@ let wordTally = splitWord.length;
 //wordTally ++
 
 //Create the guess- workspace and apply event-listeners to register selection
-
 answerCheckArray = [];
 // for (let i=0; i<= wordTally; i++) {
 //     answerCheckArray.push("");
@@ -24,58 +23,88 @@ function applyGuessDiv() {
   const guessDiv = document.createElement("div");
   guessDiv.className = "custom_class";
   const currentDiv = document.querySelector(".answerSpace");
-  document.body.appendChild(guessDiv);
+  currentDiv.appendChild(guessDiv);
 }
 for (let i = 0; i <= splitWord.length - 1; i++) {
   applyGuessDiv();
 }
+//Attempt at getting the letter to display in the yellow child div
+// function displayLetter() {
+//    let myDivs = document.getElementsByClassName("custom_class")
+//     for (let i =0; i <=splitWord.length-1;i++){
+//         myDivs.onclick = function() {
+//             this.innerHTML = letterValue
+//         }
+//     }
+// }
 
 const letterBoxes = document.getElementsByClassName("placeholders");
 // console.log(letterBoxes);
 
 let letters = Array(26).fill("");
+let value = null;
+let letterValue = null;
 function pushLetter() {
   for (let i = 0; i <= letters.length - 1; i++) {
     const letterValue = letterBoxes[i].getAttribute("data-id");
     letterBoxes[i].addEventListener("click", () => {
       letters[i] = letterValue;
-      num = letters[i];
-      answerCheckArray.push(num);
-      console.log(letters);
-      checkForMatch()
-      return num
-    })
+      value = letters[i];
+      answerCheckArray.push(value);
+    //   console.log(letters);
+      //   displayLetter()
+      checkForMatch();
+      return value;
+    });
   }
 }
-
-function checkForMatch(){
-      console.log(answerCheckArray);
-      for (let i = 0; i < wordTally; i++) {
-        if (answerCheckArray[i] === splitWord[i]) {
-          console.log("match made");
-        } else {
-          if (answerCheckArray[i] !== splitWord[i]) {
-            console.log("No Match Made");
-            answerCheckArray.shift()
-          }
+let counter = wordTally;
+function checkForMatch() {
+  console.log(answerCheckArray);
+  for (let i = 0; i < wordTally; i++) {
+    if (answerCheckArray[i] === splitWord[i]) {
+      console.log(
+        `Match Made with ${splitWord[i]}! There are ${
+          wordTally - i - 1
+        } letter slots that remain to be discovered!`);
+        if(wordTally - i -1 < 0){
+            return
         }
+    } else {
+      if (answerCheckArray[i] !== splitWord[i]) {
+        console.log(`No Match Made -- ${answerCheckArray[i]} is incorrect`);
+        // counter--
+
+
+        // were it not for the continual iteration, could use .pop to remove the incorrect answer
+
+
+        //Penalty for incorrect Guesses
+        // counter--
+        //     if( counter < 0){
+        //         console.log("PLACEHOLDER")
+        //         return
+        //     }
       }
     }
-      console.log(letters);
-    
-  
-  // let position = 0;
-  // answerCheckArray[position++] = num
-  //             console.log(answerCheckArray);
+  }
+  console.log(counter);
+  return counter;
+}
+console.log(letters);
 
-  // for (let i=0; i<= wordTally; i++){
-  //     if(answerCheckArray[i] === splitWord[i]){
-  //         console.log("match made");
-  //     }
-  // }
+// let position = 0;
+// answerCheckArray[position++] = num
+//             console.log(answerCheckArray);
+
+// for (let i=0; i<= wordTally; i++){
+//     if(answerCheckArray[i] === splitWord[i]){
+//         console.log("match made");
+//     }
+// }
 
 pushLetter();
-checkForMatch();
+// checkForMatch();
 // let position = 0;
 //     answerCheckArray[position++] = pushLetter()
 //     console.log(answerCheckArray);
